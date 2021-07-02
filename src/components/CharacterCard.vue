d8876efabe74dbaf5649d2d4db573c95f11f8d52
Sasha <zzsashaz@gmail.com>
2021-07-02 18:28:36 +0700

    alpha version done

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -3,19 +3,20 @@
     <template #cover>
       <img
         alt="Photo"
-        src="https://starwars-visualguide.com/assets/img/characters/1.jpg"
+        :src="`https://starwars-visualguide.com/assets/img/characters/${CharID}.jpg`"
       />
     </template>
-    <a-card-meta title="Имя героя"> </a-card-meta>
+    <h1 class="character-name">{{ CharName }}</h1>
+    <h3 class="character-born">{{ CharBorn }}</h3>
     <div class="like-area">
       <HeartOutlined
         v-if="!isLiked"
-        style="font-size: 30px; cursor: pointer"
+        style="font-size: 23px; cursor: pointer"
         @click="changeLike"
       />
       <HeartFilled
         v-if="isLiked"
-        style="font-size: 30px; cursor: pointer"
+        style="font-size: 23px; cursor: pointer; color: red"
         @click="changeLike"
       />
     </div>
@@ -24,6 +25,9 @@
 
 <script>
 import { HeartOutlined, HeartFilled } from "@ant-design/icons-vue";
+import { getHomeworld } from "@/hooks/main";
+import { likedCharacters } from "@/store";
+
 export default {
   name: "CharacterCard",
   components: {
@@ -32,16 +36,42 @@ export default {
   },
   props: {
     CharName: String,
-    CharBorn: String,
+    CharID: Number,
+    HomeWorld: String,
   },
   data() {
     return {
       isLiked: false,
+      CharBorn: "",
     };
+  },
+  async updated() {
+    this.CharBorn = await getHomeworld(this.HomeWorld.split("/").reverse()[1]);
+    console.log(this.CharBorn);
+  },
+  async mounted() {
+    this.CharBorn = await getHomeworld(this.HomeWorld.split("/").reverse()[1]);
+    this.isLiked = JSON.parse(window.localStorage.getItem("liked")).find(
+      (el) => el.name === this.CharName
+    );
   },
   methods: {
     changeLike() {
+      if (!this.isLiked) {
+        likedCharacters.push({
+          name: this.CharName,
+          charID: this.CharID,
+          homeland: this.HomeWorld,
+        });
+      } else {
+        let idToDelete = likedCharacters.findIndex(
+          (char) => char.name === this.CharName
+        );
+        likedCharacters.splice(idToDelete, 1);
+      }
       this.isLiked = !this.isLiked;
+      localStorage.setItem("liked", JSON.stringify(likedCharacters));
+      console.log(localStorage.getItem("liked"));
     },
   },
 };
@@ -55,5 +85,12 @@ export default {
   align-items: flex-end;
   justify-content: center;
   user-select: none;
+}
+.character-name {
+  text-align: center;
+  font-size: 20px;
+}
+.character-born {
+  text-align: center;
 }
 </style>
